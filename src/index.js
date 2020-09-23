cc346f0cfe0cd150d01b866ae6c0442484dd8f17
gwroldan <gustavowroldan@gmail.com>
2020-09-22 21:57:39 -0300

    Persistencia de Estado

--- src/index.js
+++ src/index.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import reduxThunk from 'redux-thunk';
+import { PersistGate } from 'redux-persist/integration/react'
 
 import './styles/index.css';
 import App from './containers/App';
-import reducers from './store/reducers';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-const store = createStore(
-    reducers,
-    {},
-    composeEnhancers(
-        applyMiddleware(reduxThunk)
-    )
-);
+import { store, persistor } from './store/store';
 
 ReactDOM.render(
   <Provider store = { store } >
-    <App />
+    <PersistGate loading = { null } persistor = { persistor } >
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 );
---
+++ src/store/store.js
@@ -0,0 +1,23 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import reduxThunk from 'redux-thunk';
+import reducers from "./reducers";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers)
+const store = createStore(
+    persistedReducer,
+    {},
+    composeEnhancers(
+        applyMiddleware(reduxThunk)
+    )
+);
+let persistor = persistStore(store)
+
+export { store, persistor };
